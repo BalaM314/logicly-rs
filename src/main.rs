2f41d54ed3141db90d488c08e441543f07033ed0
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2025-10-18 16:16:54 -0400

    Run simulation logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use anyhow::{Context, Result, anyhow};
 use std::{env::args, fs::File, io::Read};
 
-use crate::io::parse_xml;
+use crate::{io::parse_xml, simul::Simulation};
 
 mod io;
+mod simul;
 
 fn main() -> Result<()> {
 	let arg = args()
@@ -11,8 +12,36 @@ fn main() -> Result<()> {
 		.ok_or(anyhow!("Please specify the filename"))?;
 	let file = File::open(arg).context("Error reading file")?;
 	let mut decompressed = String::new();
-	flate2::read::DeflateDecoder::new(file).read_to_string(&mut decompressed).context("Error decompressing file")?;
-	let parsed = parse_xml(&decompressed).unwrap();
+	flate2::read::DeflateDecoder::new(file)
+		.read_to_string(&mut decompressed)
+		.context("Error decompressing file")?;
+	let parsed = parse_xml(&decompressed)?;
 	println!("{parsed:?}");
+	let mut simul: Simulation = parsed.into();
+
+	let mut inputs = simul.get_inputs_mut();
+	**inputs.get_mut("x").unwrap() = false;
+	**inputs.get_mut("y").unwrap() = false;
+	simul.update_until_done(100);
+	simul.print_outputs();
+
+	let mut inputs = simul.get_inputs_mut();
+	**inputs.get_mut("x").unwrap() = true;
+	**inputs.get_mut("y").unwrap() = false;
+	simul.update_until_done(100);
+	simul.print_outputs();
+
+	let mut inputs = simul.get_inputs_mut();
+	**inputs.get_mut("x").unwrap() = false;
+	**inputs.get_mut("y").unwrap() = true;
+	simul.update_until_done(100);
+	simul.print_outputs();
+
+	let mut inputs = simul.get_inputs_mut();
+	**inputs.get_mut("x").unwrap() = true;
+	**inputs.get_mut("y").unwrap() = true;
+	simul.update_until_done(100);
+	simul.print_outputs();
+	
 	Ok(())
 }
